50c5e27d9b646e4c165bf9d39e41ae6ff8c28b6e
Arjun Tanwar <arjunthakur900@gmail.com>
2024-01-03 18:05:11 +0530

    playwright setup

--- routers/index.js
+++ routers/index.js
@@ -7,6 +7,17 @@ import dotenv from 'dotenv';
     * @property {string} config.envPath - File path of the .env file.
     * @property {function(): boolean} config.permissionCallback - Function that controls access to the routes exposed by the Sprint Router.
     * @returns {Router} An express router.
+    * @example
+    * // Note: these configurations work for a .env file located at app root. Also the permission's callback here returns true as its merely an example.
+    // Ideally the permission's callback would contain a way to authenticate an admin and only return true if a user with admin role is authenticated.
+    // For production ALWAYS implement the permission's callback to secure the application otherwise your env file can be exposed to anyone who goes to www.example.com/sprint.
+    const currentModuleURL = import.meta.url;
+    const currentModulePath = fileURLToPath(currentModuleURL);
+    const basePath = resolve(dirname(currentModulePath), './.env');
+    app.use('/sprint', getSprintRouter({
+        envPath: basePath,
+        permissionCallback: () => true
+    }));
 */
 export const getSprintRouter = ({ envPath, permissionCallback = () => { return false; } }) => {
     const envKeyArr = [
