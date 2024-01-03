eda7ca41c3ab813e17eb50af111cac55223ad273
Arjun Tanwar <arjunthakur900@gmail.com>
2024-01-03 19:08:34 +0530

    fixing routes

--- tests/core.spec.ts
+++ tests/core.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
 
 test('hits node server successfully', async ({ page }) => {
-    // await page.goto('http://localhost:3000');
+    await page.goto('http://localhost:5173');
 });
