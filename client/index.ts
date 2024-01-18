db63de8a5216c6578bcc90d0093dbe4478e71e97
Arjun Tanwar <arjunthakur900@gmail.com>
2024-01-18 16:39:59 +0530

    updated version again

--- client/index.ts
+++ client/index.ts
@@ -1,2 +1,37 @@
-import { useSprintMailer } from './useSprintMailer'
+import React from "react";
 
+export type SprintMailResponse = {
+    status: boolean
+    mailer: Record<string, any>
+    message?: string
+    error?: string
+}
+
+//TODO add sprintmailer config (this only sends to test)
+
+export const useSprintMailer = (sprintRouterRootUrl: string) => {
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [mailer, setMailer] = React.useState<any | null>(null);
+
+    const sendMail = async() => {
+        setLoading(true);
+        setMailer(null);
+        try {
+            const res = await fetch(sprintRouterRootUrl + '/send', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            const data: SprintMailResponse = await res.json();
+            setMailer(data.mailer);
+            setLoading(false);
+            return data;
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+
+    }
+    return { loading, mailer, sendMail };
+}
